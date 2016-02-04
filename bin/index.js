3847206976414ecc0628798bfeb7ab1ce69e5656
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-02-03 22:23:25 -0200

    changes

--- bin/index.js
+++ bin/index.js
@@ -1,50 +1,6 @@
 #!/usr/bin/env node
 
 const argv = require('minimist')(process.argv.slice(2));
-const fs = require('fs');
-const open = require('open');
-const http = require('http');
-const WebSocketServer = require('ws').Server;
-const express = require('express');
-const chokidar = require('chokidar');
-const cheerio = require('cheerio');
+const server = require('../lib/server');
 
-const ROOT = process.cwd();
-const PORT = argv.port || 3000;
-
-const app = express();
-
-app.use(express.static(ROOT + '/'));
-
-app.get('/', (req, res) => res.send(_html));
-app.get('*', (req, res) => res.send(_html));
-
-const server = http.createServer(app)
-                   .listen(PORT, () => {
-                     console.log('listening on ', PORT);
-                   });
-
-const wss = new WebSocketServer({server: server});
-
-var _index = fs.readFileSync(ROOT + '/index.html').toString();
-
-var $ = cheerio.load(_index);
-
-$('body').append("<script>var ws = new WebSocket('ws://'+location.host); ws.onmessage = function(ev) {if (ev.data === 'reload') {location.reload();}}</script>");
-
-var _html = $.html();
-
-fs.writeFileSync(ROOT + '/index.html', _html);
-
-wss.on('connection', (ws) => {
-  console.log('connected');
-
-  chokidar.watch('.', {ignored: /[\/\\]\./})
-          .on('change', (event, path) => {
-            console.log('connected');
-
-            ws.send('reload');
-          });
-});
-
-open('http://localhost:' + PORT);
+server(argv);
